f47981b078858d3b17cf211e876d711e24b1dc60
Lautaro Francisco <87411204+LautaroMFrancisco@users.noreply.github.com>
2021-10-03 18:33:15 -0300

    Updated Css Path

--- Desafio 5/Pug/src/index.js
+++ Desafio 5/Pug/src/index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("view engine", "pug");
 app.set("views", "views");
