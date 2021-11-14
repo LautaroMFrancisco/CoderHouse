0bf001f37abf6e9c4c4ab74001ae6d1a85bbd88d
Lautaro Francisco <francisco.lautarom@gmail.com>
2021-11-13 22:39:15 -0300

    Updated with New Folders

---
+++ Frontend/Desafio 1/main.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    counter: 1,
+    clicks: 0,
+  },
+  methods: {
+    incrementCounter() {
+      this.counter += 1;
+    },
+    decreaseCounter() {
+      if (this.counter > 0) {
+        this.counter -= 1;
+      }
+    },
+    clickCounter() {
+      this.clicks += 1;
+    },
+  },
+  computed: {
+    powerCounter() {
+      return this.counter * this.counter;
+    },
+  },
+});
---
+++ Frontend/README.md
@@ -0,0 +1,3 @@
+# CoderHouse
+ 
+Aqui van a poder encontrar todos los desafios del Curso de Frontend de CoderHouse!
