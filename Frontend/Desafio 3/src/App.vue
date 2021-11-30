d07b60e75aa84fd50f2069c738ea7548aad351f0
Lautaro Francisco <francisco.lautarom@gmail.com>
2021-11-30 03:25:09 -0300

    Desafio 3

---
+++ Frontend/Desafio 3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-spacer></v-spacer>
+      <h1 class="cen justify-center">Waifus</h1>
+    </v-app-bar>
+
+    <v-main>
+      <TableComponent />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TableComponent from "./components/TableComponent";
+
+export default {
+  name: "App",
+
+  components: {
+    TableComponent,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Frontend/Desafio 3/src/components/TableComponent.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="waifus"
+    item-key="id"
+    class="elevation-1"
+  >
+    <template v-slot:header.id="{ header }">
+      {{ header.text.toUpperCase() }}
+    </template>
+    <template v-slot:item="{ item }">
+      <tr :class="getColor(item.id)">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.anime }}</td>
+        <td>{{ item.season }}</td>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      waifus: [
+        {
+          id: 1,
+          name: "Kigiri",
+          anime: "Danganronpa",
+          season: "Summer 2013",
+        },
+        {
+          id: 2,
+          name: "May",
+          anime: "Pokemon Advanced Generation",
+          season: "Fall 2002",
+        },
+        {
+          id: 3,
+          name: "Makise",
+          anime: "Steins;Gate",
+          season: "Spring 2011",
+        },
+        {
+          id: 4,
+          name: "Dia",
+          anime: "Sekai Saikou no Ansatsusha, Isekai Kizoku ni Tensei suru",
+          season: "Fall 2021",
+        },
+      ],
+    };
+  },
+  computed: {
+    headers() {
+      return [
+        { text: "ID", value: "id" },
+        { text: "Name", value: "name" },
+        { text: "Anime", value: "anime" },
+        { text: "Season", value: "season" },
+      ];
+    },
+  },
+  methods: {
+    getColor(id) {
+      if (id % 2 === 0) return "red-bg";
+      else return "pink-bg";
+    },
+  },
+};
+</script>
+<style>
+.red-bg {
+  display: table-row;
+  background: pink;
+  color: black;
+  font-weight: bold;
+}
+.pink-bg {
+  display: table-row;
+  background: mediumvioletred;
+  color: white;
+  font-weight: bold;
+}
+</style>
